b877355b1f7bb188f704c77bf9a4f488e8ce8050
quipcode <abdulkarim8ahmed@gmail.com>
2020-05-15 11:49:12 -0700

    finalized the project everything is/working as expected

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
-import os
+import os, json
 import hashlib
 import requests
 
-from flask import Flask, g, session, render_template, request, flash, redirect, url_for
+from flask import Flask, g, session, render_template, request, flash, redirect, url_for, jsonify
 from flask_session import Session
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-
-from flask import Flask
 from flask_bootstrap import Bootstrap
+from functools import wraps
+
+from forms import RegistrationForm, LoginForm, SearchForm, BookReviewForm
 
 
-# from helpers import login_required
-from forms import RegistrationForm, LoginForm, SearchForm
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login first")
+            return redirect(url_for('login'))
+
+    return wrap
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -40,7 +51,6 @@ def index():
 def login():
     """Sign into Tome Review"""
     form = LoginForm(request.form)
-    sform = SearchForm(request.form)
     # Forget any user_id
     session.clear()
     if request.method == "POST":
@@ -69,7 +79,6 @@ def login():
 @app.route("/logout")
 def logout():
     session.clear()
-    session['logged_in'] = False
     return redirect("/")
 
 
@@ -100,13 +109,9 @@ def register():
         return(str(e))
 
 
-# @app.route("/search")
-# def search():
-#     form = SearchForm(request.form)
-    
-#     return render_template("search.html", form=form)
 
 @app.route('/search', methods=["GET", "POST"])
+@login_required
 def search():
     form = SearchForm(request.form)
     if request.method == "POST" and form.validate():
@@ -115,18 +120,156 @@ def search():
         # https://docs.python.org/3.7/library/stdtypes.html?highlight=title#str.title
         query = query.title()
         # results = db.execute("SELECT * FROM books WHERE \ isbn LIKE :query OR \ title LIKE :query OR \ author LIKE :query RETURNING *", {"query": query})
-        results = db.execute("SELECT isbn, title, author, year FROM books WHERE \
+        books = db.execute("SELECT isbn, title, author, year FROM books WHERE \
                         isbn LIKE :query OR \
                         title LIKE :query OR \
-                        author LIKE :query LIMIT 15 ",
+                        author LIKE :query",
                         {"query": query})
        
         
-        books = results.fetchall()
+        
+        if books.rowcount == 0:
+            return render_template("error.html", message="No books were found matching that query")
+
         return render_template("search.html", books=books, form=form)
     if 'username' in session:
         return render_template("search.html", form=form)
     else:
         return redirect("/")
     
+
+@app.route("/book/<isbn>", methods=['GET','POST'])
+@login_required
+def book(isbn):
+    """ Save user review and load same page with reviews updated."""
+    form = BookReviewForm(request.form)
+    if request.method == "POST" and form.validate() :
+
+        # Save current user info
+        currentUser = session["user_id"]
+        
+        # Fetch form data
+        rating = request.form.get("rating")
+        review = request.form.get("review")
+        
+        # Search book_id by ISBN
+        row = db.execute("SELECT id FROM books WHERE isbn = :isbn",
+                        {"isbn": isbn})
+
+        # Save id into variable
+        bookId = row.fetchone() # (id,)
+        bookId = bookId[0]
+
+        # Check for user submission (ONLY 1 review/user allowed per book)
+        row2 = db.execute("SELECT * FROM reviews WHERE user_id = :user_id AND book_id = :book_id",
+                    {"user_id": currentUser,
+                        "book_id": bookId})
+
+        # A review already exists
+        if row2.rowcount == 1:
+            
+            flash('You already submitted a review for this book - Tome Reviewers are only granted 1 review per book', 'warning')
+            return redirect("/book/" + isbn)
+
+        # Convert to save into DB
+        rating = int(rating)
+
+        db.execute("INSERT INTO reviews (user_id, book_id, review, rating) VALUES \
+                    (:user_id, :book_id, :review, :rating)",
+                    {"user_id": currentUser, 
+                    "book_id": bookId, 
+                    "review": review, 
+                    "rating": rating})
+
+        # Commit transactions to DB and close the connection
+        db.commit()
+
+        flash('Review submitted!', 'info')
+
+        return redirect("/book/" + isbn)
     
+    # Take the book ISBN and redirect to his page (GET)
+    else:
+        
+        row = db.execute("SELECT isbn, title, author, year FROM books WHERE \
+                        isbn = :isbn",
+                        {"isbn": isbn})
+
+        bookInfo = row.fetchall()
+
+        """ GOODREADS reviews """
+
+        # Read API key from env variable
+        key = os.getenv("GOODREADS_KEY")
+        
+        # Query the api with key and ISBN as parameters
+        query = requests.get("https://www.goodreads.com/book/review_counts.json",
+                params={"key": key, "isbns": isbn})
+
+        # Convert the response to JSON
+        response = query.json()
+
+        # "Clean" the JSON before passing it to the bookInfo list
+        response = response['books'][0]
+
+        # Append it as the second element on the list. [1]
+        bookInfo.append(response)
+
+        """ Users reviews """
+
+         # Search book_id by ISBN
+        row = db.execute("SELECT id FROM books WHERE isbn = :isbn",
+                        {"isbn": isbn})
+
+        # Save id into variable
+        book = row.fetchone() # (id,)
+        book = book[0]
+
+        # Fetch book reviews
+        # Date formatting (https://www.postgresql.org/docs/9.1/functions-formatting.html)
+        results = db.execute("SELECT users.username, review, rating, \
+                            to_char(time, 'DD Mon YY - HH24:MI:SS') as time \
+                            FROM users \
+                            INNER JOIN reviews \
+                            ON users.id = reviews.user_id \
+                            WHERE book_id = :book \
+                            ORDER BY time",
+                            {"book": book})
+
+        reviews = results.fetchall()
+
+        return render_template("book.html", bookInfo=bookInfo, reviews=reviews, form=form)
+
+@app.route("/api/<isbn>", methods=['GET'])
+@login_required
+def api_call(isbn):
+
+    # COUNT returns rowcount
+    # SUM returns sum selected cells' values
+    # INNER JOIN associates books with reviews tables
+
+    row = db.execute("SELECT title, author, year, isbn, \
+                    COUNT(reviews.id) as review_count, \
+                    AVG(reviews.rating) as average_score \
+                    FROM books \
+                    INNER JOIN reviews \
+                    ON books.id = reviews.book_id \
+                    WHERE isbn = :isbn \
+                    GROUP BY title, author, year, isbn",
+                    {"isbn": isbn})
+
+    # Error checking
+    if row.rowcount != 1:
+        return jsonify({"Error": "Invalid ISBN || Not cataloged"}), 422
+
+    # Fetch result from RowProxy    
+    tmp = row.fetchone()
+
+    # Convert to dict
+    result = dict(tmp.items())
+
+    # Round Avg Score to 2 decimal. This returns a string which does not meet the requirement.
+    # https://floating-point-gui.de/languages/python/
+    result['average_score'] = float('%.2f'%(result['average_score']))
+
+    return jsonify(result)
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import TextField, BooleanField, StringField, PasswordField, TextAreaField, validators, SubmitField
+from wtforms import TextField, BooleanField, StringField, PasswordField, TextAreaField, validators, SubmitField, SelectField
 
 class RegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=3, max=25)])  
@@ -18,4 +18,14 @@ class LoginForm(Form):
 
 class SearchForm(Form):
     query = StringField('Search query', [validators.DataRequired()])  
+    submit = SubmitField('Submit')
+
+class BookReviewForm(Form):
+    # rating = SelectField(
+    #     choices=[1,2,3,4,5],
+    #     validators=[validators.DataRequired(), validators.NumberRange(min=1, max=5) ]
+    # )
+    rating = SelectField('State', choices=[('XX', 'Select a Rating'), ('1','1') ,('2','2'),('3', '3'),('4', '4'),('5', '5') ], default='XX', validators=[validators.DataRequired(), validators.AnyOf(['1','2','3','4','5'])])
+    # rating = SelectField(choices=[1,2,3,4,5], validators=[validators.DataRequired()], render_kw={"placeholder": "None"})
+    review = TextAreaField('Provide a review', [validators.DataRequired()])
     submit = SubmitField('Submit')
